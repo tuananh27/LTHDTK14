3720b980625a07b0047d3c9b3abc113ae50aa31e
tuananh27 <vuhoangtuananhgmail.com>
2022-01-13 16:57:59 +0700

    nộp bài lại

---
+++ src/SInhvienGDU/SinhvienTestDrive.java
@@ -0,0 +1,12 @@
+package SInhvienGDU;
+
+public class SinhvienTestDrive {
+    public static void main(String[] args) {
+        Sinhvien sinhvien = new Sinhvien();
+        sinhvien.nhap();
+        sinhvien.in();
+        
+        sinhvien.nhap();
+        sinhvien.in();
+    }   
+}
